3b3a92041aeeb28cc5e868ec4ad5c487d0da7b77
Raphael Quintanilha dos Santos <raphael_santos@id.uff.br>
2017-02-07 18:17:05 -0200

    added rspec and user scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  include ActionController::HttpAuthentication::Token
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def authenticate
+    unless authenticate_with_http_token { |token, options| User.find_by(token: token) }
+      render json: { error: 'Bad Token'}, status: 401
+    end
+  end
+
+  def not_found
+    return api_error(status: 404, errors: 'Not found')
+  end
+
 end
