dc81ac9847fbe1bf2fbb4ab7a0c212ac9c14fe29
Raphael Quintanilha dos Santos <raphael_santos@id.uff.br>
2017-02-08 01:17:57 -0200

    admin inherit from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
 
   has_secure_token
   has_secure_password
+  acts_as_paranoid
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
 end
