99cb1e9eaf1227432cffd9d9494e3a12e7917681
Raphael Quintanilha dos Santos <raphael_santos@id.uff.br>
2017-02-08 01:23:12 -0200

    added migration token on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208031943) do
+ActiveRecord::Schema.define(version: 20170208032219) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170208031943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string   "token"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
   end
 
